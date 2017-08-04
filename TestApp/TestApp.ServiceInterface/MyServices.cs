40c7de81e6616cf55d9db4acc87722d473ed62d6
Demis Bellot <demis.bellot@gmail.com>
2017-08-03 21:13:19 -0400

    Add TestApp to test Soap12ServiceClient

---
+++ TestApp/TestApp.ServiceInterface/MyServices.cs
@@ -0,0 +1,13 @@
+﻿using ServiceStack;
+using TestApp.ServiceModel;
+
+namespace TestApp.ServiceInterface
+{
+    public class MyServices : Service
+    {
+        public object Any(GetDevices request)
+        {
+            return new GetDevicesResponse();
+        }
+    }
+}
---
+++ TestApp/TestApp.Tests/SoapTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using ServiceStack;
+using TestApp.ServiceModel;
+
+namespace TestApp.Tests
+{
+    [TestFixture]
+    public class SoapTests
+    {
+        [Test]
+        public void Can_call_GetDevices_with_Soap12ServiceClient()
+        {
+            var client = new Soap12ServiceClient("http://localhost:49488/");
+
+            var response = client.Send(new GetDevices());
+        }
+    }
+}
